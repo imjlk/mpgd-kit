2812253e50fa375532f3fbbcdd48a2fd7760d791
imjlk <i@m.junglei.kim>
2026-09-25 07:02:53 +0900

    test(release): gate native foundation on packed consumer artifacts

--- native-plugins/capacitor-game-services/Package.swift
+++ native-plugins/capacitor-game-services/Package.swift
@@ -19,6 +19,7 @@ let package = Package(
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm")
             ],
-            path: "ios/Sources/CapacitorGameServices")
+            path: "ios/Sources/CapacitorGameServices",
+            resources: [.process("PrivacyInfo.xcprivacy")])
     ]
 )
